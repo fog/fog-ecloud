ece72bbe0361012011b7962de467f1980872997f
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-06-16 14:38:39 -0500

    Removed unneeded assignment

--- lib/fog/compute/ecloud/requests/ssh_key_delete.rb
+++ lib/fog/compute/ecloud/requests/ssh_key_delete.rb
@@ -9,7 +9,6 @@ module Fog
         def ssh_key_delete(uri)
           ssh_key_id = id_from_uri(uri)
 
-          ssh_key = self.data[:ssh_keys][ssh_key_id]
           self.data[:ssh_keys].delete(ssh_key_id)
           response(:body =>  nil, :status => 204)
         end
