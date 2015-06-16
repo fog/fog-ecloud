8e1af448da29200a0d33fe971f71ef7b8fe8401f
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-06-16 11:14:42 -0500

    Initial version

---
+++ lib/fog/compute/ecloud/requests/ssh_key_delete.rb
@@ -0,0 +1,19 @@
+module Fog
+  module Compute
+    class Ecloud
+      class Real
+        basic_request :ssh_key_delete, 204, "DELETE"
+      end
+
+      class Mock
+        def ssh_key_delete(uri)
+          ssh_key_id = id_from_uri(uri)
+
+          ssh_key = self.data[:ssh_keys][ssh_key_id]
+          self.data[:ssh_keys].delete(ssh_key_id)
+          response(:body =>  nil, :status => 204)
+        end
+      end
+    end
+  end
+end
