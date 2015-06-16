6feb79c50e42945311a08ad5decf1cc768e4b46f
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-06-16 14:27:58 -0500

    Added Mock support

--- lib/fog/compute/ecloud/requests/ssh_key_edit.rb
+++ lib/fog/compute/ecloud/requests/ssh_key_edit.rb
@@ -24,6 +24,20 @@ module Fog
           end
         end
       end
+
+      class Mock
+        def ssh_key_edit(data)
+          ssh_key_id = id_from_uri(data[:uri]).to_i
+          if self.data[:ssh_keys][ssh_key_id]
+            self.data[:ssh_keys][ssh_key_id][:name] = data[:name]
+            self.data[:ssh_keys][ssh_key_id][:default] = data[:default]
+            ssh_key = self.data[:ssh_keys][ssh_key_id]
+            response(:body => Fog::Ecloud.slice(ssh_key, :id, :admin_organization))
+          else
+            response(:status => 404)
+          end
+        end
+      end
     end
   end
 end
