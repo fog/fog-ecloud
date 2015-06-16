4bb7386280fbdf892a78437ac7ef710cb92ee566
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-06-16 11:44:53 -0500

    Added edit method

--- lib/fog/compute/ecloud/models/ssh_key.rb
+++ lib/fog/compute/ecloud/models/ssh_key.rb
@@ -16,6 +16,24 @@ module Fog
         end
         alias_method :destroy, :delete
 
+        def edit(options = {})
+          # Make sure we only pass what we should
+          new_options = {}
+          new_options[:uri] = href
+          if options[:name].nil?
+            new_options[:name] = name
+          else
+            new_options[:name] = options[:name]
+          end
+          if options[:default].nil?
+            new_options[:default] = default
+          else
+            new_options[:default] = options[:default]
+          end
+
+          service.ssh_key_edit(new_options).body
+        end
+
         def id
           href.scan(/\d+/)[0]
         end
