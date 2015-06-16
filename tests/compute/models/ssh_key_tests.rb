2e541fddd5c1df1bf63fd3ad2c4d80c408b376ca
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-06-16 14:26:22 -0500

    Added create, delete, and edit tests

--- tests/compute/models/ssh_key_tests.rb
+++ tests/compute/models/ssh_key_tests.rb
@@ -16,4 +16,26 @@ Shindo.tests("Fog::Compute[:#{provider}] | ssh_keys", [provider.to_s]) do
 
     returns(false) { @ssh_keys.get(ssh_key.href).nil? }
   end
+
+  tests('#create').succeeds do
+    new_key = @ssh_keys.create(name: "testing")
+    @key_id = new_key.id || nil
+    returns(false) { new_key.nil? }
+  end
+
+  tests('#edit').succeeds do
+    the_key = @ssh_keys.get(@key_id)
+    returns(false) { the_key.nil? }
+    the_key.edit(name: "more testing")
+    the_key.reload
+    returns(false) { the_key.name != "more testing" }
+  end
+
+  tests('#delete').succeeds do
+    the_key = @ssh_keys.get(@key_id)
+    returns(false) { the_key.nil? }
+    the_key.delete
+    the_key = @ssh_keys.get(@key_id)
+    returns(false) { !the_key.nil? }
+  end
 end
