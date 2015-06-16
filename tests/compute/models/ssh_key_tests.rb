3247a823a7676f765d1f0825e13ce162512f6276
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-06-16 14:49:23 -0500

    Changed to pre-1.9 hash style to keep hound happy

--- tests/compute/models/ssh_key_tests.rb
+++ tests/compute/models/ssh_key_tests.rb
@@ -7,31 +7,31 @@ Shindo.tests("Fog::Compute[:#{provider}] | ssh_keys", [provider.to_s]) do
   @admin_organization.reload
   @ssh_keys = @admin_organization.ssh_keys
 
-  tests('#all').succeeds do
+  tests("#all").succeeds do
     returns(false) { @ssh_keys.empty? }
   end
 
-  tests('#get').succeeds do
+  tests("#get").succeeds do
     ssh_key = @ssh_keys.first
 
     returns(false) { @ssh_keys.get(ssh_key.href).nil? }
   end
 
-  tests('#create').succeeds do
-    new_key = @ssh_keys.create(name: "testing")
+  tests("#create").succeeds do
+    new_key = @ssh_keys.create({:name => "testing"})
     @key_id = new_key.id || nil
     returns(false) { new_key.nil? }
   end
 
-  tests('#edit').succeeds do
+  tests("#edit").succeeds do
     the_key = @ssh_keys.get(@key_id)
     returns(false) { the_key.nil? }
-    the_key.edit(name: "more testing")
+    the_key.edit({:name => "more testing"})
     the_key.reload
     returns(false) { the_key.name != "more testing" }
   end
 
-  tests('#delete').succeeds do
+  tests("#delete").succeeds do
     the_key = @ssh_keys.get(@key_id)
     returns(false) { the_key.nil? }
     the_key.delete
